f6ba2bdab47b309aeda242d441709e5411c690cf
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2024-05-12 15:19:55 +0200

    Closing API Client after usage to prevent error messages

--- kuinterlink/kubernetes_client.py
+++ kuinterlink/kubernetes_client.py
@@ -6,13 +6,14 @@ import json
 from typing import Literal
 
 import kubernetes_asyncio as k8s
+from kubernetes_asyncio.client.api_client import ApiClient
 from fastapi import HTTPException
 
 
 __API_GROUPS__ = dict(
     core=k8s.client.CoreV1Api,
     custom_object=k8s.client.CustomObjectsApi,
-    batch = k8s.client.BatchV1Api,
+    batch=k8s.client.BatchV1Api,
 )
 
 ApiGroup = Literal['core', 'custom_object']
@@ -31,23 +32,24 @@ def initialize_k8s():
 @asynccontextmanager
 async def kubernetes_api(group: ApiGroup = 'core'):
     logger = logging.getLogger("Kubernetes")
-    try:
-        yield __API_GROUPS__[group]()
-    except k8s.client.exceptions.ApiException as exception:
+    async with ApiClient() as api_client:
         try:
-            body = json.loads(exception.body)
-        except json.JSONDecodeError:
-            logger.error("HTTP error not returning a JSON.")
+            yield __API_GROUPS__[group](api_client)
+        except k8s.client.exceptions.ApiException as exception:
+            try:
+                body = json.loads(exception.body)
+            except json.JSONDecodeError:
+                logger.error("HTTP error not returning a JSON.")
+                logger.error(traceback.print_exc())
+                raise HTTPException(exception.status, f"({exception.reason}) {exception.body}")
+            else:
+                logger.error(f"Error {exception.status} ({exception.reason})")
+                message = body.get("message", "Kubernetes error")
+                logger.error(message)
+                logger.error(traceback.print_exc())
+                raise HTTPException(exception.status, message)
+        except Exception as e:
+            logger.error(str(e))
             logger.error(traceback.print_exc())
-            raise HTTPException(exception.status, f"({exception.reason}) {exception.body}")
-        else:
-            logger.error(f"Error {exception.status} ({exception.reason})")
-            message = body.get("message", "Kubernetes error")
-            logger.error(message)
-            logger.error(traceback.print_exc())
-            raise HTTPException(exception.status, message)
-    except Exception as e:
-        logger.error(str(e))
-        logger.error(traceback.print_exc())
-        raise HTTPException(500, "Unknown kubernetes error")
+            raise HTTPException(500, "Unknown kubernetes error")
 
